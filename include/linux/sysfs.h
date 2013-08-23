a111269b34b430a677ea11041ca08d8e10e77b81
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-08-23 15:02:01 -0700

    sysfs: create __ATTR_WO()
    This creates the macro __ATTR_WO() for write-only attributes, instead of
    having to "open define" them.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: HridayHS <hridaysharma42@gmail.com>

--- include/linux/sysfs.h
+++ include/linux/sysfs.h
@@ -86,6 +86,11 @@ struct attribute_group {
 	.store	= _name##_store,					\
 }
 
+#define __ATTR_WO(_name) {						\
+	.attr	= { .name = __stringify(_name), .mode = S_IWUSR },	\
+	.store	= _name##_store,					\
+}
+
 #define __ATTR_RW(_name) __ATTR(_name, (S_IWUSR | S_IRUGO),		\
 			 _name##_show, _name##_store)
 

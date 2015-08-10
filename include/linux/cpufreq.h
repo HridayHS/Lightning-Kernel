37e32ae8aa898eb4805f5646a5febce67a9dfa1c
DespairFactor <matthewalex@outlook.com>
2015-08-10 10:58:17 +0000

    BioShock (CPU Governor)
    Signed-off-by: AudioGod <audiogod@sonic-developers.com>
    Signed-off-by: HridayHS <hridaysharma42@gmail.com>
    
    Conflicts:
    	arch/arm64/configs/cyanogenmod_tomato-64_defconfig
    	drivers/cpufreq/Kconfig
    	include/linux/cpufreq.h
    
    Conflicts:
    	arch/arm64/configs/cyanogenmod_tomato-64_defconfig

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -470,9 +470,14 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
 extern struct cpufreq_governor cpufreq_gov_elementalx;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
+extern struct cpufreq_governor cpufreq_gov_bioshock;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
 #endif
 
 /*********************************************************************
+
  *                     FREQUENCY TABLE HELPERS                       *
  *********************************************************************/
 

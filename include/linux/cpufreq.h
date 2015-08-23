7a470ec53a1f75e090871dc3a2d9355e45e82785
HridayHS <hridaysharma42@gmail.com>
2015-08-23 20:21:18 +0530

    Add PegasusQ Governor
    Signed-off-by: HridayHS <hridaysharma42@gmail.com>
    
    Conflicts:
    	arch/arm64/configs/cyanogenmod_tomato-64_defconfig
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    
    Signed-off-by: HridayHS <hridaysharma42@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -455,6 +455,9 @@ extern struct cpufreq_governor cpufreq_gov_hyper;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
 extern struct cpufreq_governor cpufreq_gov_blu_active;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
+extern struct cpufreq_governor cpufreq_gov_pegasusq;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_pegasusq)
 #endif
 
 /*********************************************************************

3177885f8fa3686fc668af7add68070715c102c0
HridayHS <hridaysharma42@gmail.com>
2015-08-30 15:30:16 +0530

    Add Darkness Governor
    Signed-off-by: HridayHS <hridaysharma42@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Makefile
    
    Signed-off-by: HridayHS <hridaysharma42@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -446,7 +446,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_alucard)
->>>>>>> fb44d58... Add Alucard Governor
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_darkness)
 #endif
 
 /*********************************************************************

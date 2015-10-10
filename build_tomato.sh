480507de10c3658422d9ec2b28c626911aa0be68
HridayHS <hridaysharma42@gmail.com>
2015-10-10 15:26:07 +0530

    more ignores

--- build_tomato.sh
+++ build_tomato.sh
@@ -15,63 +15,7 @@
  # Please maintain this if you use this script or any part of it
  #
 KERNEL_DIR=$PWD
-KERN_IMG=$KERNEL_DIR/arch/arm64/boot/Image
-DTBTOOL=$KERNEL_DIR/dtbToolCM
-BUILD_START=$(date +"%s")
-blue='\033[0;34m'
-cyan='\033[0;36m'
-yellow='\033[0;33m'
-red='\033[0;31m'
-nocol='\033[0m'
-# Modify the following variable if you want to build
-export CROSS_COMPILE="/home/hriday/toolchain/ubertc/bin/aarch64-linux-android-"
-export ARCH=arm64
-export SUBARCH=arm64
-export KBUILD_BUILD_USER="HridayHS"
-export KBUILD_BUILD_HOST="LK"
-STRIP="/home/hriday/toolchain/ubertc/bin/aarch64-linux-android-strip"
-MODULES_DIR=$KERNEL_DIR/modules_tomato
+DTBTOOL=$KERNEL_DIR/tools/dtbToolCM
 
-compile_kernel ()
-{
-echo -e "$blue***********************************************"
-echo    "            Compiling Lightning Kernel              "
-echo -e "***********************************************$nocol"
-rm $KERN_IMG
-make cyanogenmod_tomato-64_defconfig
-make Image -j4
-make dtbs -j4
-make modules -j4
-if ! [ -a $KERN_IMG ];
-then
-echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
-exit 1
-fi
 $DTBTOOL -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
-strip_modules
-}
 
-strip_modules ()
-{
-echo "Copying modules"
-rm $MODULES_DIR/*
-find . -name '*.ko' -exec cp {} $MODULES_DIR/ \;
-cd $MODULES_DIR
-echo "Stripping modules for size"
-$STRIP --strip-unneeded *.ko
-zip -9 modules *
-cd $KERNEL_DIR
-}
-
-case $1 in
-clean)
-make ARCH=arm64 -j4 clean mrproper
-rm -rf $KERNEL_DIR/arch/arm/boot/dt.img
-;;
-*)
-compile_kernel
-;;
-esac
-BUILD_END=$(date +"%s")
-DIFF=$(($BUILD_END - $BUILD_START))
-echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"

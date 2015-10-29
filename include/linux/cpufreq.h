c19c977f0fe9faec046b9d16c2dd74155f84c0d9
HridayHS <hridaysharma42@gmail.com>
2015-10-29 15:35:16 +0530

    Add Intellidemand Governor
    Signed-off-by: HridayHS <hridaysharma42@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -486,6 +486,9 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
 extern struct cpufreq_governor cpufreq_gov_barry_allen;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
 #endif
 
 /*********************************************************************

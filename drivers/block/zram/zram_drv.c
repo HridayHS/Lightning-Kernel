b03b2275677d8575d3113857a06f1568b8d9ed5e
HridayHS <hridaysharma42@gmail.com>
2015-12-15 16:13:23 +0530

    zram_drv.c: fix compile
    Signed-off-by: HridayHS <hridaysharma42@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -52,7 +52,7 @@ static const char *default_compressor = "lz4";
 #define ALLOC_ERROR_LOG_RATE_MS 1000
 
 /* Module params (documentation at end) */
-static unsigned int num_devices = 4;
+static unsigned int num_devices = 1;
 
 #ifdef CONFIG_STATE_NOTIFIER
 static struct notifier_block notif;

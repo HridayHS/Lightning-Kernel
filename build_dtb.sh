15733a4150ecadcb5910c3ec83f837863ccd1825
Hriday Sharma <hridaysharma42@gmail.com>
2016-05-29 10:53:01 +0000

    Import features
    Signed-off-by: Hriday Sharma <hridaysharma42@gmail.com>

---
+++ build_dtb.sh
@@ -0,0 +1,21 @@
+ #
+ # Copyright © 2015, hridaysharma42@gmail.com
+ #
+ # Custom build script
+ #
+ # This software is licensed under the terms of the GNU General Public
+ # License version 2, as published by the Free Software Foundation, and
+ # may be copied, distributed, and modified under those terms.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # Please maintain this if you use this script or any part of it
+ #
+KERNEL_DIR=$PWD
+DTBTOOL=$KERNEL_DIR/tools/dtbToolCM
+
+$DTBTOOL -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
+
